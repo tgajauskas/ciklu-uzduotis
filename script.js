89eb36a49d21a9d828d031ba734e65f2060ac4ad
tgajauskas <149151624+tgajauskas@users.noreply.github.com>
2023-11-19 15:53:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1. Not sure ar gerai
+
+var vardas = "Tomas";
+for (var i = 0; i < 10; i++) {
+    console.log(vardas);
+}
+
+// 2. Not sure ar gerai
+
+var vardas1 = "Tomas";
+var kartai = 50;
+for (var i = 0; i < kartai; i++) {
+    console.log(vardas1);
+}
+
+// 3. 
+
+var vardas2 = "Tomas";
+for (var i = 0; i < 15; i++) {
+    console.log(i + ". " + vardas2);
+}
+
+// 4.
+
+console.log("Skaičiavimas:");
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+}
